8ad657eaa5f0a3923c8dfed3681f511dfb5567ad
Martin Kerschbaumer <maff32@gmx.com>
2021-01-12 10:41:51 +0100

    Allow to specify which algorithm to use Tolerate already existing repo on server Install zip Require docker run to be executed successfully Create result zip

--- connect_ssh.sh
+++ connect_ssh.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 function usage() {
-	echo "$0 <VM instance IP> [ssh-key-file]"
-	echo "The first parameter is required and the second one (ssh key file) is optional."
-	echo "e.g. $0 123.456.789.123 ~/.ssh/id_ed25519"
+	echo "$0 <VM instance IP> <algorithm> [ssh-key-file]"
+	echo "The first two parameters are required and the third one (ssh key file) is optional."
+	echo "e.g. $0 123.456.789.123 agent57 ~/.ssh/id_ed25519"
+	echo "e.g. $0 123.456.789.123 dqn ~/.ssh/id_ed25519"
 	echo
 	echo "The default username can be overridden by setting SSH_USERNAME"
 }
@@ -15,7 +16,8 @@ if [[ "$1" == "--help" || -z "$1" ]]; then
 fi
 
 VM_IP=$1
-SSH_KEY_FILE=$2
+ALGORITHM=$2
+SSH_KEY_FILE=$3
 
 # Add SSH key if provided
 # -n means non-empty
@@ -33,7 +35,7 @@ echo
 
 echo "Installing docker on machine"
 TARGET="$SSH_USERNAME@$VM_IP"
-ssh "$TARGET" -- "sudo apt update && sudo apt install -y docker.io git" || exit
+ssh "$TARGET" -- "sudo apt update && sudo apt install -y docker.io git zip" || exit
 
 echo "Downloading required docker image"
 ssh "$TARGET" -- sudo docker pull mkerschbaumer/openai-gym || exit
@@ -45,7 +47,7 @@ echo "Using git repo '$GIT_REPO' (set GIT_REPO to override)"
 
 # Temporary directory for the cloned repository
 echo "Cloning repo containing the code"
-ssh "$TARGET" -- git clone "$GIT_REPO" repo || exit
+ssh "$TARGET" -- "sudo rm -fr repo && git clone $GIT_REPO repo" || exit
 
 if [ -z "$GIT_BRANCH" ]; then
 	GIT_BRANCH=pong
@@ -53,15 +55,32 @@ fi
 echo "Checking out git branch '$GIT_BRANCH' (set GIT_BRANCH to override)"
 ssh "$TARGET" -- "cd repo && git checkout -q $GIT_BRANCH && cd .." || exit
 
-echo "Training Agent57"
-ssh "$TARGET" -- 'sudo docker run --rm --workdir /code --env AGENT57=1 -v $PWD/tmp:/code/tmp_Pong-v4 -v $PWD/repo/agent:/code/agent -v $PWD/repo/examples:/code/examples mkerschbaumer/openai-gym python3 examples/atari_pong.py'
+# Construct algorithm-specific parts of docker run command
+if [ "$ALGORITHM" == "agent57" ]; then
+	ALGORITHM_SPECIFIC='--env AGENT57=1 -v $PWD/tmp:/code/tmp_Pong-v4'
+elif [ "$ALGORITHM" == "dqn" ]; then
+	ALGORITHM_SPECIFIC='--env DQN=1 -v $PWD/tmp:/code/tmp'
+else
+	1>&2 echo "Unknown algorithm '$ALGORITHM': must either be 'agent57' or 'dqn'!"
+	exit 1
+fi
+
+echo "Training using algorithm $ALGORITHM"
+REMAINING_VOLUME_MOUNTS='-v $PWD/repo/agent:/code/agent -v $PWD/repo/examples:/code/examples'
+SSH_COMMAND="sudo docker run --rm --workdir /code $ALGORITHM_SPECIFIC $REMAINING_VOLUME_MOUNTS mkerschbaumer/openai-gym python3 examples/atari_pong.py"
+echo "Executed SSH command: '$SSH_COMMAND'"
+ssh "$TARGET" -- "$SSH_COMMAND" || exit
 
 RESULTS_DIR=results
 if [ ! -d "$RESULTS_DIR" ]; then
-	echo "Creating results directory"
+	echo "Creating local results directory"
 	mkdir "$RESULTS_DIR" || exit
 fi
 
+RESULT_ZIP_FILE="$ALGORITHM.zip"
+echo "Creating zip file '$RESULT_ZIP_FILE' on VM"
+ssh "$TARGET" -- "zip -r $RESULT_ZIP_FILE \$(find tmp -type f -name '*.json')" || exit
+
 echo "Downloading results from VM"
-scp -r "$TARGET:tmp/*.json" "$RESULTS_DIR" || exit
+scp -r "$TARGET:$RESULT_ZIP_FILE" "$RESULTS_DIR" || exit
 
